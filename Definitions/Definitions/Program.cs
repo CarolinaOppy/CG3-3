2d32077ae6cb172977fd06adb3066b57aefeec9d
carocosta87 <carocosta87@gmail.com>
2018-08-08 18:14:38 -0500

    Finished

---
+++ Definitions/Definitions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Definitions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Statement - defines a variable//
+            var month = 10;
+
+            //Expresion - creates a term//
+            if (month > 10)
+
+            //Operator - the action piece ("+") //
+            month = month + 2;
+
+            //Operand - the pieces being acted on ("month" and "2")//
+            month = month + 2;
+
+        }
+    }
+}
